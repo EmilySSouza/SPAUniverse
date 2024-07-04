f3d9156071780ccf1f08ea0d66e1aacdb630fc5b
Emily <85910442+EmilySSouza@users.noreply.github.com>
2024-07-04 11:13:10 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ import "./active.js";
 
 const router = new Router();
 
-router.add("/SPAUniverse/", "pages/home.html");
-router.add("/SPAUniverse/universe", "pages/universe.html");
-router.add("/SPAUniverse/exploration", "pages/exploration.html");
-router.add("/SPAUniverse/404", "pages/404.html")
+router.add("/", "pages/home.html");
+router.add("/universe", "pages/universe.html");
+router.add("/exploration", "pages/exploration.html");
+router.add("404", "pages/404.html")
 
 router.handle();
 
