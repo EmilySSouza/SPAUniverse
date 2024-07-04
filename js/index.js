a56a7569fc057f30103ef2c919ac4aa0d4ab005b
Emily <85910442+EmilySSouza@users.noreply.github.com>
2024-07-04 10:51:28 -0300

    Update index.js
    Modificação para testar o SPA no Github Pages

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ import "./active.js";
 
 const router = new Router();
 
-router.add("/", "/SPAUniverse/pages/home.html");
-router.add("/universe", "/SPAUniverse/pages/universe.html");
-router.add("/exploration", "/SPAUniverse/pages/exploration.html");
-router.add(404, "/SPAUniverse/pages/404.html")
+router.add("/SPAUniverse/", "pages/home.html");
+router.add("/SPAUniverse/universe", "pages/universe.html");
+router.add("/SPAUniverse/exploration", "pages/exploration.html");
+router.add("/SPAUniverse/404", "pages/404.html")
 
 router.handle();
 
